81de9a072d4b3a247273e896cf7d2d65b647770c
Colin Walker <colinw@aviobook.aero>
2024-07-12 22:22:10 +0200

    Added IPs to outputs for further use in Ansible.

---
+++ terraform_modules/environment/outputs.tf
@@ -0,0 +1,14 @@
+output "public-app-ip" {
+  value = aws_eip.application
+}
+output "private-app-ip" {
+  value = aws_instance.application.private_ip
+}
+output "public-db-ip" {
+  value = aws_eip.database
+}
+output "private-db-ip" {
+  value = aws_instance.database.private_ip
+}
+
+
