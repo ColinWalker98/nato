135a60f936769f1449ddcb37354c0579cb120ec2
Colin Walker <colinw@aviobook.aero>
2024-07-12 22:40:01 +0200

    Added version constraints to setup of environment.

--- terraform_modules/environment/providers.tf
+++ terraform_modules/environment/providers.tf
@@ -4,3 +4,25 @@ provider "aws" {
     role_arn     = "arn:aws:iam::743558884073:role/terraform-operator"
   }
 }
+
+terraform {
+  required_version = "1.9.2"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.58.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.2"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
+}
