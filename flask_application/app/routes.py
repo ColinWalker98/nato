67def976f8f8c062b43248971d20e6e88a7ebced
Colin Walker <colinw@aviobook.aero>
2024-07-13 16:45:53 +0200

    Created initial basic flask application

--- flask_application/app/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import jsonify, request
-from app import app, mongo
-
-@app.route('/')
-def index():
-    return "Welcome to the Flask app!"
---
+++ flask_application/config.py
@@ -0,0 +1,4 @@
+debug_mode = os.getenv('DEBUG_MODE', True)
+mongo_user_name = os.getenv('MONGO_USER_NAME','wcol')
+mongo_user_password = os.getenv('MONGO_USER_PASSWORD','wcol')
+mongo_uri = os.getenv('MONGO_URI','localhost:27017')
