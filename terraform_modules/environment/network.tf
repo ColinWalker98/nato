495c7e91ef9aaa50d9f992d0dda206181fb87d44
Colin Walker <colinw@aviobook.aero>
2024-07-13 03:49:09 +0200

    Consolidated variable resources in the vars.tf file, added types and descriptions to variables.

--- terraform_modules/environment/network.tf
+++ terraform_modules/environment/network.tf
@@ -1,69 +1,6 @@
 #--------------------------------------------------------------
 # This module creates all resources necessary for a vpc
 #--------------------------------------------------------------
-locals {
-  cidr_block = var.custom_cidr == "" ? var.cidr_blocks[var.stage] : var.custom_cidr
-  subnet_pub_cidrs = length(var.custom_subnet_pub_cidrs) == 0 ? var.subnet_pub_cidrs[var.stage] : var.custom_subnet_pub_cidrs
-  subnet_priv_cidrs = length(var.custom_subnet_priv_cidrs) == 0 ? var.subnet_priv_cidrs[var.stage] : var.custom_subnet_priv_cidrs
-}
-
-variable "vpc_name" {
-  description = "Name of the vpc."
-  default = "vpc"
-}
-
-variable "sub_count" {
-  description = "Amount of subnets (Subnet group per availability zone that is used)."
-  default     = "2"
-}
-
-variable "cidr_blocks" {
-  description="Main CIDR Blocks for vpc."
-  type    = object({dev: string, acc: string, prod: string})
-  default = {
-    "prod"   = "10.0.0.0/16", #- with subnets [10.0.1.0/24, 10.0.2.0/24, 10.0.3.0/24, 10.0.4.0/24 10.0.5.0/24, 10.0.5.0/24, ....]
-    "acc"    = "10.1.0.0/16", #- with subnets [10.1.1.0/24, 10.1.2.0/24, 10.1.3.0/24, 10.1.4.0/24 10.1.5.0/24, 10.1.5.0/24, ....]
-    "dev"    = "10.2.0.0/16", #- with subnets [10.2.1.0/24, 10.2.2.0/24, 10.2.3.0/24, 10.2.4.0/24 10.2.5.0/24, 10.2.5.0/24, ....]
-  }
-}
-
-variable "subnet_pub_cidrs" {
-  description = "Public subnet ranges (requires 3 entries)."
-  type    = object({dev: list(string), acc: list(string), prod: list(string)})
-  default = {
-    "prod"   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24","10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
-    "acc"    = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24","10.1.7.0/24", "10.1.8.0/24", "10.1.9.0/24"]
-    "dev"    = ["10.2.1.0/24", "10.2.2.0/24", "10.2.3.0/24","10.2.7.0/24", "10.2.8.0/24", "10.2.9.0/24"]
-  }
-}
-
-variable "subnet_priv_cidrs" {
-  description = "Private subnet ranges (requires 3 entries)."
-  type    = object({dev: list(string), acc: list(string), prod: list(string)})
-  default = {
-    prod   = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24","10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
-    acc    = ["10.1.4.0/24", "10.1.5.0/24", "10.1.6.0/24","10.1.10.0/24", "10.1.11.0/24", "10.1.12.0/24"]
-    dev    = ["10.2.4.0/24", "10.2.5.0/24", "10.2.6.0/24","10.2.10.0/24", "10.2.11.0/24", "10.2.12.0/24"]
-  }
-}
-
-variable "custom_cidr" {
-  description = "Option to provide custom CIDR range if desired, this overrides the default provided lists based on the environment stage."
-  type = string
-  default = ""
-}
-
-variable "custom_subnet_pub_cidrs" {
-  description = "Option to override with custom public subnet CIDR."
-  type = list(string)
-  default = []
-}
-
-variable "custom_subnet_priv_cidrs" {
-  description = "Option to override with custom private subnet CIDR."
-  type = list(string)
-  default = []
-}
 
 data "aws_availability_zones" "available" {}
 
