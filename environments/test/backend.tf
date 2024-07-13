50f31227c81c6c0e01c08184dddf63fd3127e928
Colin Walker <colinw@aviobook.aero>
2024-07-13 02:17:33 +0200

    Added ansible dir blueprint

--- environments/test/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    key            = "test/terraform.tfstate"
-    bucket         = "terraform-remote-state-nato"
-    region         = "eu-central-1"
-    encrypt        = true
-    dynamodb_table = "terraform-remote-state-lock-nato"
-  }
-}
--- environments/test/test.tf
+++
@@ -1,5 +0,0 @@
-data "aws_availability_zones" "available" {}
-
-output "availability_zones" {
-  value = data.aws_availability_zones.available.names
-}
